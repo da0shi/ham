f08f4ea20d9097cf000aaff837e4b11562a0feec
takayukioda <takayuki@odashi.me>
2019-01-14 16:38:46 +0900

    feat: add inputs for the record

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (Model, init, main)
 
 import Browser
-import Html exposing (Html, div, form, h1, input, label, li, option, select, text, ul)
+import Html exposing (Html, div, form, h1, h2, input, label, li, option, select, text, ul)
 import Html.Attributes exposing (type_, value)
 import Html.Events exposing (onClick, onInput)
 
@@ -20,13 +20,12 @@ main =
 
 type alias Model =
     { accounts : List Account
-    , accountInput : Account
     }
 
 
 init : Model
 init =
-    Model [] Account
+    Model [ { id = 1, name = "三井住友目白店" }, { id = 2, name = "三井住友渋谷駅前店" }, { id = 3, name = "Sony Bank" } ]
 
 
 type alias Account =
@@ -40,34 +39,59 @@ type alias Account =
 
 
 type Msg
-    = InputAccount String
-    | AddAccount
+    = None
+
+
+
+--= InputAccount String
+--| AddAccount
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        InputAccount name ->
-            { model | accountInput = { id = List.length model.accounts, name = name } }
-
-        AddAccount ->
-            { model | accounts = List.append model.accounts [ model.accountInput ] }
+        None ->
+            model
 
 
 
+--InputAccount name ->
+--    { model | accountInput = { id = List.length model.accounts, name = name } }
+--AddAccount ->
+--    { model | accounts = List.append model.accounts [ model.accountInput ] }
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Add Account" ]
+        [ h1 [] [ text "HAM" ]
+        , h2 [] [ text "Spent money" ]
+        , form []
+            [ div []
+                [ label [] [ text "Date" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "Account" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "Price" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "What" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "Amount (optional)" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "Where" ]
+                , input [ type_ "text" ] []
+                , label [] [ text "Notes" ]
+                , input [ type_ "text" ] []
+                ]
+            ]
+        , h2 [] [ text "Add Account" ]
         , form []
             [ div []
                 [ label [] [ text "Account" ]
-                , input [ type_ "text", onInput InputAccount ] []
+                , input [ type_ "text" ] []
                 ]
-            , div [] [ input [ type_ "submit", onClick AddAccount ] [ text "Add" ] ]
+            , div [] [ input [ type_ "submit" ] [ text "Add" ] ]
             ]
         , div []
             [ ul [] (accountsToList model.accounts) ]
@@ -76,4 +100,4 @@ view model =
 
 accountsToList : List Account -> List (Html msg)
 accountsToList accounts =
-    List.map (\account -> li [] [ text account.name ]) accounts
+    List.map (\acc -> li [] [ text (String.fromInt acc.id ++ ": " ++ acc.name) ]) accounts
