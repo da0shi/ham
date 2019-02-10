11dad689a3e3f6e1cf2019bb08fdc32631cc8211
takayukioda <takayuki@odashi.me>
2019-02-10 18:58:43 +0900

    feat: add middleware

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { RecordModule } from './records/record.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { RecordController } from './records/record.controller';
 
 @Module({
   imports: [RecordModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(RecordController);
+  }
+}
